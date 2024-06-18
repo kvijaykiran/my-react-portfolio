53649351deb3e91a43f0ac239d768e1744c17c87
kvijaykiran <35195585+kvijaykiran@users.noreply.github.com>
2024-06-18 17:14:12 -0500

    Setting up vite for predeploy and deploy
    Setting up vite for predeploy and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-react-portfolio/',
   plugins: [react()],
+  build: {
+    outDir: 'build', // Output directory
+    sourcemap: true, // Enable source maps for debugging
+    rollupOptions: {
+      output: {
+        // Custom output options if needed
+      },
+    },
+  }
 })
