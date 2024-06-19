7e1868d246f2b1712d24150f638280e59c406c61
kvijaykiran <35195585+kvijaykiran@users.noreply.github.com>
2024-06-19 09:00:46 -0500

    Update vite.config.ts
    Using defaults in vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-react-portfolio/',
-  publicDir: 'public',
   plugins: [react()],
 });
 
