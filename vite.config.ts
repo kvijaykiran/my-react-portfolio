5ac104758247898bf530ef6ab96f3a9fd5fa1e4f
kvijaykiran <35195585+kvijaykiran@users.noreply.github.com>
2024-06-18 23:17:55 -0500

    Updated deploy.yml and vite.config.ts
    Updated deploy.yml and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/my-react-portfolio/',
   publicDir: 'public',
   plugins: [react()],
 });
