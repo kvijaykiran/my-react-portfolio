97d8d4b976accf5195a5ec6a3db3d5a9b1362391
kvijaykiran <35195585+kvijaykiran@users.noreply.github.com>
2024-06-19 15:40:39 -0500

    added repo-name explicitly
    added repo-name explicitly

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-/*
+
 // Ensure the correct type is used
 export default defineConfig({
-  base: import.meta.env.VITE_BASE_URL || '/',
+  //base: 'import.meta.env.VITE_BASE_URL || '/'',
+  base: '/my-react-portfolio/',
   plugins: [react()],
 });
-*/
 
+
+/*
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on the current mode ('development', 'production', etc.)
@@ -19,3 +21,4 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
   };
 });
+*/
