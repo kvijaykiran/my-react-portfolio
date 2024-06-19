e63342f2b3749cfd4712e944369cfe8dfcfeee30
kvijaykiran <35195585+kvijaykiran@users.noreply.github.com>
2024-06-19 14:01:33 -0500

    Changes to accommodate import.meta.env
    Changes to accommodate import.meta.env. Some files moved around too.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+/*
+// Ensure the correct type is used
 export default defineConfig({
+  base: import.meta.env.VITE_BASE_URL || '/',
   plugins: [react()],
+});
+*/
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env file based on the current mode ('development', 'production', etc.)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [react()],
+  };
 });
