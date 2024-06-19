c21f310e072b8d0e0624db824bde82ef22a4bbc7
kvijaykiran <35195585+kvijaykiran@users.noreply.github.com>
2024-06-19 10:42:52 -0500

    Simple relative path for assets
    Simple relative path for assets. Vite not managing paths. Want to check if this works.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import BabylonScene from "./BabylonScene";
 
 const App: React.FC = () => {
+  console.log("vite base url: ", import.meta.env.VITE_BASE_URL);
+
   return (
     <div className="App">
       <BabylonScene />
