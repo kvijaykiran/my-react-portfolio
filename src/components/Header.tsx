1b19d1c7a0dd477718a2041f39995979580f4243
kvijaykiran <35195585+kvijaykiran@users.noreply.github.com>
2024-06-26 15:11:01 -0400

    Added submenus
    Added submenus. I commented out code that associated submenu entry to animated front view. CameraController.tsx is created but is unused for now.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,38 @@
 import React, { useState, useRef, useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { setPosition, setTarget } from '../redux/cameraSlice';
+// import { setPosition } from '../redux/cameraSlice';
 import '../styles/_header.scss';
+// import { Vector3 } from '@babylonjs/core/Maths/math.vector';
 
 const Header: React.FC = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [submenuOpen, setSubmenuOpen] = useState< { [key: string]: boolean}>({});
   const dropdownRef = useRef<HTMLDivElement>(null);
+  // const dispatch = useDispatch();
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
 
+  const toggleSubmenu = (key: string) => {
+    setSubmenuOpen(prev => ({ ...prev, [key]: !prev[key]}));
+  };
+
   const handleClickOutside = (event: MouseEvent) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
       setDropdownOpen(false);
+      setSubmenuOpen({});
     }
   };
+
+  // const handleMenuClick = (item: string) => {
+  //   if (item === 'Front') {
+  //     dispatch(setPosition(new Vector3(0, 0, 10))); // Front view position
+  //     dispatch(setTarget(new Vector3(0, 0, 0))); // Target position
+  //   }
+  //   // Add more conditions for other menu items if needed
+  // };
 
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
@@ -31,8 +50,29 @@ const Header: React.FC = () => {
           Menu
           {dropdownOpen && (
             <div className="dropdown-content">
-              <div className="dropdown-item">Views</div>
-              <div className="dropdown-item">Geometries</div>
+              <div className="dropdown-item" onMouseEnter={() => toggleSubmenu('a')} onMouseLeave={() => toggleSubmenu('a')}>Views
+                {submenuOpen['a'] && (
+                  <div className='submenu-content'>
+                  <div className="submenu-item">Front</div>
+                  <div className="submenu-item">Left</div>
+                  <div className="submenu-item">Top</div>
+                  <div className="submenu-item">Isometric</div>
+                  </div>
+                )}
+              </div>
+
+              <div className="dropdown-item" onMouseEnter={() => toggleSubmenu('b')} onMouseLeave={() => toggleSubmenu('b')}>Geometries
+              {submenuOpen['b'] && (
+                  <div className='submenu-content'>
+                  <div className="submenu-item">Shell</div>
+                  <div className="submenu-item">Revolve extrusion</div>
+                  <div className="submenu-item">Assembly</div>
+                  <div className="submenu-item">Shaded</div>
+                  </div>
+                )}
+
+              </div>
+
             </div>
           )}
         </div>
--- src/redux/cameraSlice.ts
+++ src/redux/cameraSlice.ts
@@ -11,11 +11,13 @@ interface Vector3State {
 interface CameraState {
   position: Vector3State;
   rotation: Vector3State;
+  //target: Vector3State;
 }
 
 const initialState: CameraState = {
   position: {x: 0, y: 0, z: -10},
   rotation: {x: -Math.PI / 2, y: -Math.PI / 2.5, z: 0},
+  //target: {x: 0, y: 0, z: 0},
 };
 
 const cameraSlice = createSlice({
@@ -28,9 +30,13 @@ const cameraSlice = createSlice({
     setRotation: (state, action: PayloadAction<Vector3>) => {
       state.rotation = action.payload;
     },
+    // setTarget: (state, action: PayloadAction<Vector3>) => {
+    //   state.target = action.payload;
+    // }
   },
 });
 
+// export const { setPosition, setRotation, setTarget } = cameraSlice.actions;
 export const { setPosition, setRotation } = cameraSlice.actions;
 
 export default cameraSlice.reducer;
